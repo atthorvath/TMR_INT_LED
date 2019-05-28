fa8aa65c43958b0d8bf3d6997a6742ac1597edaf
atthorvath <horvath.attila@gmx.ch>
2019-05-28 17:32:07 +0200

    changed prescaler and period value

--- TMR_INT_LED/Src/stm32l1xx_it.c
+++ TMR_INT_LED/Src/stm32l1xx_it.c
@@ -207,7 +207,7 @@ void TIM2_IRQHandler(void)
   /* USER CODE END TIM2_IRQn 0 */
   HAL_TIM_IRQHandler(&htim2);
   /* USER CODE BEGIN TIM2_IRQn 1 */
-  HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_5);
+  HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_5); //toggles LED LD2 with period of 500ms
   /* USER CODE END TIM2_IRQn 1 */
 }
 
